cf41115c3f858399e6bafeb7c4bec4017bca3f7c
Shelomentsev Dmitriy <shelomentsev@protonmail.com>
2018-09-21 21:07:24 +0300

    Oh boy, here we go

---
+++ src/go.mod
@@ -0,0 +1,3 @@
+module github.com/shelomentsevd/url-shortener
+
+require github.com/gorilla/mux v1.6.2
---
+++ src/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"test"
+)
+
+func main() {
+	r := mux.NewRouter()
+	_ = r
+}
