df420077c3a15ff9434269ca345543aa35e36579
Shelomentsev Dmitriy <shelomentsev@protonmail.com>
2019-09-09 00:56:45 +0300

    handlers draft

--- src/handler/create.go
+++ src/handler/create.go
@@ -1,13 +1,39 @@
 package handler
 
-import "net/http"
+import (
+	"controller"
+	"errors"
+	"net/http"
+	"strings"
+)
 
-type Create struct {}
+const (
+	paramURL  = "url"
+	paramJSON = "json"
+)
 
-func NewCreate() *Create {
-	return &Create{}
+var ErrTooShort = errors.New("URL is too short")
+
+type Create struct {
+	Controller controller.Controller
+}
+
+func NewCreate(controller controller.Controller) *Create {
+	return &Create{
+		Controller: controller,
+	}
 }
 
 func (handler *Create) ServeHTTP(rw http.ResponseWriter, req *http.Request) {
+	query := req.URL.Query()
 
+	pJSON := strings.ToLower(query.Get(paramJSON)) == "true"
+	pURL := query.Get(paramURL)
+
+	key, err := handler.Controller.Save(pURL)
+	if err != nil {
+		// TODO: 500
+	}
+
+	// TODO: 200 OK
 }
